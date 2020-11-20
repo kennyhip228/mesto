966efe816e6db270f733f40fb3643bce1e08b232
Nikita Sobolev <nikitasobl1@gmail.com>
2020-11-20 18:20:34 +0300

    feat: добавить функционал сайту

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+const popup = document.querySelector('.popup');
+const profileButtonNode = document.querySelector('.profile__edit');
+const popupCloseButtonNode = popup.querySelector('.popup__close-icon');
+const popupSaveButtonNode = popup.querySelector('.popup__save');
+
+profileButtonNode.addEventListener('click', handleHeaderButtonClick);
+popupCloseButtonNode.addEventListener('click', handlePopupCloseButtonClick);
+popupSaveButtonNode.addEventListener('click', handlePopupCloseButtonClick);
+
+
+function handleHeaderButtonClick() {
+
+    popup.classList.add('popup_opened');
+
+}    
+
+function handlePopupCloseButtonClick() {
+
+    popup.classList.remove('popup_opened');
+}
+
+const popupFormNode = document.querySelector('.popup__form');
+
+popupFormNode.addEventListener('submit', formSubmitHandler);
+
+const profileNameNode = document.querySelector('.profile__name');
+const popupNameInputNode = document.querySelector('.popup__name');
+const profileJobNode = document.querySelector('.profile__job');
+const popupJobInputNode = document.querySelector('.popup__job')
+
+function formSubmitHandler (evt) {
+
+    evt.preventDefault();
+
+    profileJobNode.textContent = popupJobInputNode.value;
+    profileNameNode.textContent = popupNameInputNode.value;
+
+}
+
+popupNameInputNode.value = profileNameNode.textContent;
+popupJobInputNode.value = profileJobNode.textContent;
